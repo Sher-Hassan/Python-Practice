08e9d348c2685fb509f7ee65a3bf8e492416a2da
Sher Hassan <sherhassan739@gmail.com>
2026-08-16 11:58:21 +0500

    Practicing problem solving

---
+++ patternsPractice.py
@@ -0,0 +1,89 @@
+# for x in range(0, 5):
+#     print("\n")
+#     for y in range(0, 5):
+#         print ("*", end=" ")
+# * * * * * 
+
+# * * * * * 
+
+# * * * * * 
+
+# * * * * * 
+
+# * * * * * 
+
+# for x in range(1, 6):
+#     print("\n")
+#     for y in range(x):
+#         print("*", end=" ")
+
+# * 
+
+# * * 
+
+# * * * 
+
+# * * * * 
+
+# * * * * * 
+
+
+# for x in range(1, 6):
+#     print("\n")
+#     for y in range(1, x+1):
+#         print(y, end=" ")
+
+# 1 
+
+# 1 2 
+
+# 1 2 3 
+
+# 1 2 3 4 
+
+# 1 2 3 4 5 
+
+# for x in range(1, 6):
+#     print("\n")
+#     for y in range(1, x+1):
+#         print(x, end=" ")
+
+# 1 
+
+# 2 2 
+
+# 3 3 3 
+
+# 4 4 4 4 
+
+# 5 5 5 5 5 
+
+# for x in range(0, 5):
+#     print("\n")
+#     for y in range(x, 5):
+#         print("*", end=" ")
+
+# * * * * * 
+
+# * * * * 
+
+# * * * 
+
+# * * 
+
+# * 
+
+# for x in range(6, 1, -1):
+#     print("\n")
+#     for y in range(1, x):
+#         print(y, end=" ")
+
+# 1 2 3 4 5 
+
+# 1 2 3 4 
+
+# 1 2 3 
+
+# 1 2 
+
+# 1 
