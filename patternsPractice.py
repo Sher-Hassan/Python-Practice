399485e6ea692d4d86e8d9844fe6bfdadb3a4b88
Sher Hassan <sherhassan739@gmail.com>
2026-08-17 07:26:22 +0500

    DSA Practice patterns

--- patternsPractice.py
+++ patternsPractice.py
@@ -143,3 +143,79 @@
 #       * * *       
 
 #         *       
+
+# for x in range(1, 6):
+#     print("\n")
+#     for y in range(x):
+#         print("*", end=" ")
+# for x in range(0, 5):
+#     print("\n")
+#     for y in range(4-x):
+#         print("*", end=" ")
+
+# * 
+
+# * * 
+
+# * * * 
+
+# * * * * 
+
+# * * * * * 
+
+# * * * * 
+
+# * * * 
+
+# * * 
+
+# * 
+
+# for x in range(1, 6):
+#     print("\n")
+#     for y in range(x):
+#         if(y % 2 == 0):
+#             print("1", end=" ")
+#         else:
+#             print("0", end=" ")
+# 1 
+
+# 1 0 
+
+# 1 0 1 
+
+# 1 0 1 0 
+
+# 1 0 1 0 1 
+
+# for x in range(1, 5):
+#     print("\n")
+#     for y in range(x):
+#         print(y+1, end=" ")
+#     for y in range(8-2*x):
+#         print(" ", end=" ")
+#     for y in range(x, 0, -1):
+#         print(y, end=" ")
+# 1             1 
+
+# 1 2         2 1 
+
+# 1 2 3     3 2 1 
+
+# 1 2 3 4 4 3 2 1 
+
+# num = 1
+# for x in range(0, 5):
+#     print("\n")
+#     for y in range(x+1):
+#         print(num, end=" ")
+#         num+= 1
+# 1 
+
+# 2 3 
+
+# 4 5 6 
+
+# 7 8 9 10 
+
+# 11 12 13 14 15 
